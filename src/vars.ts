bbae919817ca735540c0ef5d73df81729f5e5bd8
BART! <bart@bartweb.cz>
2021-06-21 15:23:17 +0200

    feat(exceptions): check paths and return exceptions

--- src/vars.ts
+++ src/vars.ts
@@ -1,5 +1,7 @@
 import { Options } from './interfaces/global.interface';
 
+export const APP_NAME = 'svelte-sitemap';
+
 export const DOMAIN = 'https://example.com';
 
 export const OPTIONS: Options = { resetTime: false, debug: false, changeFreq: 'weekly' };
