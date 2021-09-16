1496621cfc2022b38d033710eb5b02ad38abd930
BART! <bart@bartweb.cz>
2021-09-16 18:18:00 +0200

    test(outdir): test outDir properly [fix #13]

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,12 +1,20 @@
 import { prepareData } from '../src/helpers/global.helper';
 import { PagesJson } from '../src/interfaces/global.interface';
 
+const options: { outDir?: string } = {};
+
+const cliArgs = process.argv.filter((x) => x.startsWith('--outDir='))[0];
+if (cliArgs?.split('=')[1]) {
+  options.outDir = cliArgs?.split('=')[1];
+}
+console.log('JEST OPTIONS:', options);
+
 const sortbyPage = (json: PagesJson[]) => json.sort((a, b) => a.page.localeCompare(b.page));
 
 // Sitemap
 describe('Create JSON model', () => {
   test('Default sitemap', async () => {
-    const json = await prepareData('https://example.com');
+    const json = await prepareData('https://example.com', { ...options });
 
     expect(sortbyPage(json)).toMatchObject(
       sortbyPage([
@@ -45,7 +53,10 @@ describe('Create JSON model', () => {
   });
 
   test('Sitemap with frequency', async () => {
-    const json = await prepareData('https://example.com', { changeFreq: 'daily' });
+    const json = await prepareData('https://example.com', {
+      ...options,
+      changeFreq: 'daily'
+    });
 
     expect(sortbyPage(json)).toMatchObject(
       sortbyPage([
@@ -84,7 +95,7 @@ describe('Create JSON model', () => {
   });
 
   test('Sitemap with reset time', async () => {
-    const json = await prepareData('https://example.com', { resetTime: true });
+    const json = await prepareData('https://example.com', { ...options, resetTime: true });
 
     const today = new Date().toISOString().split('T')[0];
 
@@ -126,7 +137,11 @@ describe('Create JSON model', () => {
 });
 
 test('Sitemap ignore **/page2', async () => {
-  const json = await prepareData('https://example.com', { ignore: '**/page2', debug: true });
+  const json = await prepareData('https://example.com', {
+    ...options,
+    ignore: '**/page2',
+    debug: true
+  });
 
   expect(sortbyPage(json)).toMatchObject(
     sortbyPage([
@@ -150,7 +165,11 @@ test('Sitemap ignore **/page2', async () => {
 });
 
 test('Sitemap ignore Page1', async () => {
-  const json = await prepareData('https://example.com', { ignore: 'page1', debug: true });
+  const json = await prepareData('https://example.com', {
+    ...options,
+    ignore: 'page1',
+    debug: true
+  });
 
   expect(sortbyPage(json)).toMatchObject(
     sortbyPage([
@@ -179,7 +198,10 @@ test('Sitemap ignore Page1', async () => {
 });
 
 test('Add trailing slashes', async () => {
-  const json = await prepareData('https://example.com/', { trailingSlashes: true });
+  const json = await prepareData('https://example.com/', {
+    ...options,
+    trailingSlashes: true
+  });
 
   expect(sortbyPage(json)).toMatchObject(
     sortbyPage([
@@ -217,40 +239,75 @@ test('Add trailing slashes', async () => {
   );
 });
 
-test('Output dir', async () => {
-  const json = await prepareData('https://example.com', { outDir: 'build' });
+test('Add trailing slashes and ignore page2', async () => {
+  const json = await prepareData('https://example.com/', {
+    ...options,
+    trailingSlashes: true,
+    ignore: 'page2'
+  });
 
   expect(sortbyPage(json)).toMatchObject(
     sortbyPage([
       {
-        page: 'https://example.com',
+        page: 'https://example.com/',
         changeFreq: '',
         lastMod: ''
       },
       {
-        page: 'https://example.com/page1',
+        page: 'https://example.com/page1/',
         changeFreq: '',
         lastMod: ''
       },
       {
-        page: 'https://example.com/page2',
+        page: 'https://example.com/page1/subpage1/',
         changeFreq: '',
         lastMod: ''
+      }
+    ])
+  );
+});
+
+test('Add trailing slashes + ignore subpage2 + reset time', async () => {
+  const json = await prepareData('https://example.com/', {
+    ...options,
+    trailingSlashes: true,
+    ignore: 'subppage2',
+    resetTime: true
+  });
+
+  const today = new Date().toISOString().split('T')[0];
+
+  expect(sortbyPage(json)).toMatchObject(
+    sortbyPage([
+      {
+        page: 'https://example.com/',
+        changeFreq: '',
+        lastMod: today
       },
       {
-        page: 'https://example.com/page1/subpage1',
+        page: 'https://example.com/page1/',
         changeFreq: '',
-        lastMod: ''
+        lastMod: today
       },
       {
-        page: 'https://example.com/page2/subpage2',
+        page: 'https://example.com/page2/',
         changeFreq: '',
-        lastMod: ''
+        lastMod: today
       },
       {
-        page: 'https://example.com/page2/subpage2/subsubpage2',
+        page: 'https://example.com/page1/subpage1/',
         changeFreq: '',
-        lastMod: ''
+        lastMod: today
+      },
+      {
+        page: 'https://example.com/page2/subpage2/',
+        changeFreq: '',
+        lastMod: today
+      },
+      {
+        page: 'https://example.com/page2/subpage2/subsubpage2/',
+        changeFreq: '',
+        lastMod: today
       }
     ])
   );
