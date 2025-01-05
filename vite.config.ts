8a1e928281fc82e3ce87d36665c052fdd26e24f5
BART! <bart@bartweb.cz>
2025-01-05 14:21:34 +0100

    test: add vitest coverage

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,19 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'c8'
+      provider: 'v8', // or 'istanbul'
+      exclude: [
+        '**/node_modules/**',
+        '**/demo.ts',
+        '**/index.ts',
+        '**/dist/**',
+        '**/config/**',
+        '**/*.config.{js,ts}',
+        '**/*.test.{js,ts,jsx,tsx}',
+        '**/*.d.ts',
+        '**/vars.helper.ts',
+        '**/tests/**'
+      ]
     }
   }
 });
